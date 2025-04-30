9131977fd6bfeb044ec94939aad2e9701d0ca7b9
Tony <tony@qa.com>
2025-04-30 11:18:06 +0100

    Changes to jenkinsfile

--- src/index.js
+++ src/index.js
@@ -15,3 +15,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+function Dummy(input){
+  var sql = "SELECT * FROM Customers Where name ='" + input + "';"
+}
